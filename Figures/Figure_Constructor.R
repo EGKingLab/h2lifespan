f2110f1cb3fb8e85d1dee1090775dd95d502841a
Kevin Middleton <middletonk@missouri.edu>
2018-05-25 14:41:54 -0500

    Update code for figure 4

--- Figures/Figure_Constructor.R
+++ Figures/Figure_Constructor.R
@@ -14,9 +14,13 @@ ggsave(filename = "Figure_3.png", plot = P, width = 6.9, height = 3)
 # Figure 4
 load("Lifespan_GxE.Rda")
 load("Fecundity_GxE.Rda")
+load("Lifespan_h2.Rda")
+load("Lifespan_correlation.Rda")
 
-P <- plot_grid(Lifespan_GxE, Fecundity_GxE, nrow = 2, ncol = 2,
-               labels = c("a.", "b.", "c.", "d"),
+P <- plot_grid(Lifespan_GxE, Fecundity_GxE, 
+               Lifespan_h2, Lifespan_correlation,
+               nrow = 2, ncol = 2,
+               labels = c("a.", "b.", "c.", "d."),
                label_size = 10)
 P
 ggsave(filename = "Figure_4.png", plot = P, width = 6.9, height = 6.9)
