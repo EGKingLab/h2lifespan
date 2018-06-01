eb12c2319b235f658db87635108ede9fbef2c7ab
Elizabeth Kin <kingeg@missouri.edu>
2018-06-01 11:36:37 -0500

    Update figure 3

--- Figures/Figure_Constructor.R
+++ Figures/Figure_Constructor.R
@@ -1,15 +1,24 @@
 library(tidyverse)
 library(cowplot)
+library(survival)
+library(survminer)
 
 # Figure 3
 load("KM_plot.Rda")
 load("Fec_line_plot.Rda")
+load(file="ttrait_plot.rda")
 
-P <- plot_grid(KM_plot, Fec_line_plot, nrow = 1, ncol = 2,
-               labels = c("a.", "b."),
-               label_size = 10)
+top_row <- plot_grid(KM_plot, ttrait, labels = c('a.', 'c.'), align = 'h',
+                     label_size = 10)
+P<-plot_grid(top_row, Fec_line_plot, labels = c('', 'b.'), 
+                   ncol = 1,hjust=0, label_size = 10)
+
+
+#P <- plot_grid(KM_plot, ttrait,Fec_line_plot, nrow = 1, ncol = 2,
+#               labels = c("a.", "b.","c."),
+#               label_size = 10)
 P
-ggsave(filename = "Figure_3.png", plot = P, width = 6.9, height = 3)
+ggsave(filename = "Figure_3.png", plot = P, width = 6.9, height = 6.9)
 
 # Figure 4
 load("Lifespan_GxE.Rda")
